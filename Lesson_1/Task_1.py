efb2d7fe43620ab252f116e2d91280033f745267
DerypaskoTaras <70661921+DerypaskoTaras@users.noreply.github.com>
2020-09-05 11:41:39 +0300

    Add files via upload

---
+++ Lesson_1/Task_1.py
@@ -0,0 +1,5 @@
+n = int(input())
+N = list(i for i in range(n))
+for i in N:
+    if i % 2 == 0:
+        print(i, end=' ')
---
+++ Lesson_1/Task_2.py
@@ -0,0 +1,10 @@
+Count_Capitals = {
+    'Belarus': 'Minsk',
+    'Ukraine': 'Kyiv',
+    'Egipt': 'Cairo',
+    'France': 'Paris'
+}
+Coutries = ['Estonia', 'Greece', 'India', 'Belarus', 'Latvia', 'Ukraine']
+for i in Coutries:
+    if i in Count_Capitals:
+        print(Count_Capitals[i])
