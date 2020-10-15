958a71b1e5af12f3ea3526e8d266fbae2858f300
DerypaskoTaras <70661921+DerypaskoTaras@users.noreply.github.com>
2020-10-15 13:56:37 +0300

    Delete models.py

--- Home_Work/Restful_API/models.py
+++
@@ -1,8 +0,0 @@
-import mongoengine as me
-
-me.connect('RestAPIUsers')
-
-
-class User(me.Document):
-    first_name = me.StringField(min_length=1, max_length=128)
-    last_name = me.StringField(min_length=1, max_length=128)
